48034b19e5a72ac6ccc8a09d1410a97a3bfa5210
Anna Mathieu <annamathieupro@gmail.com>
2025-12-04 11:07:46 +0100

    filtres nombres

---
+++ 04.12.R
@@ -0,0 +1,22 @@
+
+# filtres nombres : pour les nombres suivies d'une lettre
+
+test = "2007menaces covid19 acteur.rice -acteur 2010-2020 agri-alimentaire et2022-2027 egalim6-lutte 1-structuration"
+
+gestion_nombres <- function(text) {
+
+  text <- gsub("\\.", " ", x =  text, perl = TRUE)                   # mettre des espaces à la place des points
+  text <- gsub("(?<=[0-9])(?=[A-Za-z])|(?<=[A-Za-z])(?=[0-9])", " ", x =  text, perl = TRUE)   # mettre des espaces s'il y a un chiffre avant une lettre minuscule (ex : 2007menaces) ou si il y a un chiffre après une lettre minuscule (ex : covid19)
+  text <- gsub("(?<=[0-9])-(?=[A-Za-z])|(?<=[A-Za-z])-(?=[0-9])|(?<=[0-9])-(?=[0-9])", " ", text, perl = TRUE) # chiffre'-'lettre ou lettre'-'chiffre ou chiffre'-'chiffre
+
+  text <- gsub(" -", " ", x = text)                                  # supprimer les tirets en début de mots
+
+  return(text)
+
+}
+
+
+# filtres
+
+test = gestion_nombres(test)
+test
