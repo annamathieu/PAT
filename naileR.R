d0422569c2d91357c6de687eace76c8b61e31c8d
Anna Mathieu <annamathieupro@gmail.com>
2025-12-15 17:27:51 +0100

    naile R config update

--- naileR.R
+++ naileR.R
@@ -21,3 +21,40 @@ data(car_alone)
                                                                            generate = FALSE)
       cat(res_nail_textual )
       
+      gemini_generate(res_nail_textual)
+   
+         
+mots.clust <- data.frame(mots.clust)
+mots.clust$clust <- seq(1,6)
+      
+
+
+res = nail_textual(dataset = clust[,1:2], num.var = 2, num.text = 1,
+                  
+                  introduction = "A study on Territorial food systems is done and we want to find the topics discussed in the description of each Territorial food system, we use topic modelling and latent dirichlet allocation. We consolidate our topics by using multiple executions of the algorithm and perform MFA to find strong forms in our topics.",
+                  request = "We want to automatically put a name on those topics based on the words caracterising them. Please give a name to each topic suming up most of the words caracterising it. Do it in French. Only give la Liste des Noms de Groupe Attribués",
+, 
+                  isolate.groups = F, drop.negative = T, generate = F)
+res
+
+descr <- gemini_generate(res)
+
+
+
+Sys.setenv(GEMINI_API_KEY = "AIzaSyDtqqOHs_t2EiVfh7SnmXMtuyIkBilS_0Q")
+
+?gemini_generate
+
+Sys.setenv(GEMINI_API_KEY = "AIzaSyCl1Sm2rtMwXfBfAGw9n2TZMYP")
+gemini_generate(
+  prompt = "Say hello in one short sentence.",
+  model  = "gemini-2.5-flash",
+  verbose = TRUE
+)
+
+Sys.setenv(GEMINI_API_KEY = "AIzaSyCl1Sm2rtMwXfBfAGw9n2TZMYP-aolgoow")
+
+
+
+
+
