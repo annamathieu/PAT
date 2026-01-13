5b450cc725a9eebd2cb5a76b7047b5a6713276f8
Anna Mathieu <annamathieupro@gmail.com>
2026-01-13 18:53:30 +0100

    update functionnalities topic Identifier

--- topic_IdentifieR.R
+++ topic_IdentifieR.R
@@ -11,9 +11,11 @@
   # nombre de lda réalisées = nb.iter 
   # format_x 
   # taille_min_text : nombre de termes lemmatisés min pour conserver le doc
-  #
-  # 
-  # 
+  # api_key = clé api pour requetes nailR
+  # limit_inf : seuil supérieur du nombre de docs dans lequel un doc apparait pour qu'un terme doit dépasser pour être conservé dans le vocabulaire 
+  # nb.frex = 20 : nombre de termes frex caractéristiques de chaque topic de chaque modele retenus
+  # frex.threshold = 0.5, seuil frex , entre 0 et 1 : 0 = fréquence 100 % et 1 = exclusivité 100 % 
+
 
 # Output 
   # résumé des settings 
@@ -42,13 +44,13 @@
 
 # liste stop words supp : liste perso de stop words 
 
+# hyperparamètre filtre mfa df 
 
+# 
 
 ######################
 
 
-library(stm)        # lda 
-library(FactoMineR) # Analyses factorielles et classification 
 
 
 
@@ -58,18 +60,29 @@ nb.iter = 10
 seeds = sample(1:9999, nb.iter, replace = F)
 
 topicIdentifieR <- function ( X, 
-                              format_X, 
-                              nb.topics = k, 
+                              nb.topics, 
                               nb.iter = 10, 
                               taille_min_text = 20,
-                              
-                              stopwords = "base",
-                              liste_stopwords_supp = c(NULL, stopwords_supp), 
+                              api_key, 
                               preprocessing = TRUE, 
-                              language = c("fr", "english"), 
+
+                              liste_stopwords_supp = NULL, 
+                              language = c("fr", "english"),
                               
+                              
+                              limit.inf = 1, 
+                              nb.frex = 20, 
+                              frex.threshold = 0.5, 
+                              ncp = 5,
+                              
+                              introduction_llm = NULL
                               )
 {
+  
+  library(stm)        # lda 
+  library(FactoMineR) # Analyses factorielles et classification 
+  library(mixr)       # get lexicon pour lemmatisation 
+  library(NaileR)     # pour requetes LLM
   
   #################
   # Etape 0 : progra défensive et types 
@@ -80,9 +93,11 @@ topicIdentifieR <- function ( X,
   #################
   # Gestion du type de tableau de données 
   
+  
     # format de base : deux colonnes avec 1) n°/nom du texte 2) texte
     colnames(X) = c("id", "text")
   
+  if (preprocessing ==TRUE)  {
     # nettoyage 
     X$text <- gsub("(?<=[a-z])(?=[A-Z])", " ", x =  X$text, perl = TRUE) # décole MAJ collées à  minuscules (précédées par des minuscules)
     X$text <- tolower(str_squish(X$text)) # supp db espaces  + on passe en minuscules
@@ -114,7 +129,10 @@ topicIdentifieR <- function ( X,
       # on retire les stop words
     filter <- paste0("\\b(", paste(stopwords, collapse = "|"), ")\\b")
     X$text <- str_squish(str_replace_all(string=X$text, filter,"")) 
-
+    
+    print("--------------------------------------------")
+    print("Data cleaning : Done")
+}
     
     # tokenisation 
     res.tokens <- quanteda::tokens(X$text,
@@ -129,9 +147,11 @@ topicIdentifieR <- function ( X,
     )
     
     # lemmatisation 
-    if (language =="fr"){
+    if (language == "english"){
+      language = "en"  
+    } 
       
-    lexique <- mixr::get_lexicon(language = "fr")[,1:2]
+    lexique <- mixr::get_lexicon(language = language)[,1:2]
     colnames(lexique) = c("text", "lemma")
     lexique <- lexique[!duplicated(lexique$text), ]
     
@@ -139,7 +159,6 @@ topicIdentifieR <- function ( X,
     df_X_long$lemma <- ifelse(is.na(df_X_long$lemma), df_X_long$text, df_X_long$lemma)
     df_X_long <- df_X_long[,c(1,3)]  
     colnames(df_X_long) = c("id", "word")
-    }
     
     # filtre de taille de chaque doc : avec taille_min_text
     df_X_long <- df_X_long %>% 
@@ -155,6 +174,10 @@ topicIdentifieR <- function ( X,
     df_X_long$id <- as.numeric(gsub("text","", df_X_long$id))
     df_X_long <- df_X_long %>% arrange(id)
 
+    print("--------------------------------------------")
+    print("Tokenization and lemmatization : Done")
+    
+    
   #################
   # Construction du vocabulaire 
   vocab <- unique(df_X_long$word)
@@ -177,7 +200,7 @@ topicIdentifieR <- function ( X,
   names(documents) <- unique_docs
   
   df_stm <- stm::prepDocuments(documents, vocab, 
-                       lower.thresh = 1,  # remove words appearing in only 1 doc
+                       lower.thresh = limit.inf,  # remove words appearing in only 1 doc
                        upper.thresh = Inf)
   
   documents <- df_stm$documents
@@ -202,16 +225,25 @@ topicIdentifieR <- function ( X,
   }
   
     # récupération des FREX 
-  ######################################
-  #############⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
-  #####################################
-  words_frex
+  words_frex <- list()
   
+  for (model in seq_along(models)){
+    frex_mat <- t(stm::labelTopics(models[[model]], n = nb.frex, frexweight = frex.threshold)$frex)
+    # n: choix du nombre de frex , frexweight : optimisation frequence / exclusivité
+    
+    words_frex[[model]] <- vector("list", ncol(frex_mat))
+    
+    for ( topic in seq_len(ncol(frex_mat))  ) {
+      words_frex[[model]][[topic]] <- frex_mat[,topic]
+    }
+  }
+   
   
-  words_frex <- lapply(X= models, function(x) as.list(t(stm::labelTopics(models[[1]], n = 20, frexweight = 0.5)$frex)))   # n: choix du nombre de frex , frexweight : optimisation frequence / exclusivité
+    # récupération des frex uniques
   words_frex_unique <- unique(unlist(words_frex))
   
-  
+  print("--------------------------------------------")
+  print("Topic modelling models : Done")
   
     #################
   # Fortification des topics 
@@ -239,30 +271,99 @@ topicIdentifieR <- function ( X,
     }
   }
   
+    # filtrage : supp les mots de faible fréquence 
+    df_mfa$freq <- rowSums(df_mfa)
+    df_mfa <- df_mfa %>% filter(df_mfa$freq>=3) %>% select(-"freq")
+    
+
+    # MFA
+    res.mfa <- MFA(base = df_mfa, 
+                   group = rep(nb.topics,nb.iter),
+                   type = rep("f",nb.iter), 
+                   name.group = paste0("lda",seq(1:nb.iter)),
+                   graph = F 
+    
+    ) 
+    
   
-  
+    barplot(res.mfa$eig[,2])
+    
+    res.mfa <- MFA(base = df_mfa, 
+                   group = rep(nb.topics,nb.iter),
+                   type = rep("f",nb.iter), 
+                   name.group = paste0("lda",seq(1:nb.iter)),
+                   graph = F, 
+                   ncp = ncp 
+                   
+    )
+    
+    res.hcpc <- FactoMineR::HCPC(res = res.mfa, nb.clust = -1, graph = F)
+    
+    df_topics_clust <- data.frame(cbind(
+                              word = rownames(df_mfa),
+                              clust = res.hcpc$data.clust$clust, 
+                              dim1 = res.mfa$ind$coord[,1], 
+                              dim2 = res.mfa$ind$coord[,2]))
+
+      # liste des mots des topics fortifiés 
+    words_topics_fortifies <- unname(split(df_topics_clust$word, df_topics_clust$clust))
+    
+    
+    print("--------------------------------------------")
+    print("Fortification of strong forms topics : Done")
+
+    
   #################
   # Identfication des topics automatiquement avec modele de langage 
-  
-  
-  
-  
+    Sys.setenv(GEMINI_API_KEY = api_key)
+    
+    cat(gemini_generate(nail_textual(dataset = df_topics_clust[,1:2],num.text = 1, num.var = 2, 
+
+                      introduction = c(introduction_llm,"A study on a topic is achieved and we want to find the topics discussed in the given documents. We use structural topic modelling with variational Expectation Maximisation algorithms. We consolidate our topics by using multiple executions of the algorithm and perform MFA to find strong forms in our topics."),
+                      
+                      request = "We want to automatically put the best name on each of those topics based on the words caracterising them. Please give a name to each topic suming up most of the words caracterising it. Do all of the explanations and name of topics in the language of the words. Only give the list of asigned topic and a short description of each.",
+                      isolate.groups = F, drop.negative = T, generate = F)
+                      )
+        )
+
+
   # Sortie de la fct
+    
+    # voir tous les jeux de données que je peux sortir en output 
   
+  output <- list(X = X, 
+                 X_long_preprocessed = df_X_long, 
+                 X_tokenised = res.tokens,
+                 frex_terms = words_frex,
+                 df_mfa = df_mfa, 
+                 df_topics_clust = df_topics_clust,
+                 words_topics_fortifies= words_topics_fortifies,
+                 res.mfa = res.mfa,
+                 res.hcpc = res.hcpc
+                 )
   
-  
-  
+  return(output)
 }
   
+
+
+topicIdentifieR(X = textdata, 
+                nb.topics = 9, 
+                nb.iter = 10, 
+                taille_min_text = 20, 
+                api_key = "AIzaSyA1UCATqGqUpQuVRJBPEKTcilR9H-OF-g4", 
+                language = "fr"  )
   
-  
-  
-  
-  
-  
-topicIdentifieR(X = textdata)  
-  
-  
-  
-  
-  
+
+
+# hyperparamètres pour le test 
+language = "fr"
+nb.iter = 10
+nb.topics = 9
+limit.inf = 1
+taille_min_text = 20
+X = textdata
+nb.frex = 20  
+frex.threshold = 0.5
+ncp = 5
+
