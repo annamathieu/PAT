4eb628a54a8bdde5abefce7f80accb9a489cd5f5
Anna Mathieu <annamathieupro@gmail.com>
2026-01-28 15:35:54 +0100

    coherence methodo

---
+++ Cohérence_Typologie_PAT.R
@@ -0,0 +1,6 @@
+# Cohérence de notre typologie 
+
+## Nous allons réaliser notre typologie sur 200 PAT au hasard, puis 200 autres, afin de voir si : 
+  # les clusters obtenus sont les mêmes (desc var)
+  # les individus présents dans les deux typologies se placent dans les mêmes clusters 
+
